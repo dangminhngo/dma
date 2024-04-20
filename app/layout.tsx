8f01bd313dd1ef4fe47ecc09f10fc1e54d89ab7d
dangminhngo <dangminhngo.dev@gmail.com>
2024-04-20 10:26:43 +0700

    fix: remove classnames from layout body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,7 @@ export default async function RootLayout({
   const session = await getServerAuthSession()
   return (
     <html lang="en">
-      <body
-        className={sans.className}
-      >
+      <body className={sans.className}>
         <Providers session={session}>{children}</Providers>
       </body>
     </html>
